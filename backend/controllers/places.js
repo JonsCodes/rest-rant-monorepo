bf0ca947d79a65054d98a427034dca8e227ee78b
“JonsCodes” <johnalyashai@me.com>
2023-09-14 21:03:00 -0700

    edited files/migrated/seeded completed project

--- backend/controllers/places.js
+++ backend/controllers/places.js
@@ -4,6 +4,11 @@ const db = require("../models");
 const { Place, Comment, User } = db;
 
 router.post("/", async (req, res) => {
+  if (req.currentUser?.role !== "admin") {
+    return res
+      .status(403)
+      .json({ message: "You are not allowed to add a place" });
+  }
   if (!req.body.pic) {
     req.body.pic = "http://placekitten.com/400/400";
   }
@@ -45,6 +50,11 @@ router.get("/:placeId", async (req, res) => {
 });
 
 router.put("/:placeId", async (req, res) => {
+  if (req.currentUser?.role !== "admin") {
+    return res
+      .status(403)
+      .json({ message: "You are not allowed to edit places" });
+  }
   let placeId = Number(req.params.placeId);
   if (isNaN(placeId)) {
     res.status(404).json({ message: `Invalid id "${placeId}"` });
@@ -65,6 +75,11 @@ router.put("/:placeId", async (req, res) => {
 });
 
 router.delete("/:placeId", async (req, res) => {
+  if (req.currentUser?.role !== "admin") {
+    return res
+      .status(403)
+      .json({ message: "You are not allowed to delete places" });
+  }
   let placeId = Number(req.params.placeId);
   if (isNaN(placeId)) {
     res.status(404).json({ message: `Invalid id "${placeId}"` });
@@ -95,31 +110,26 @@ router.post("/:placeId/comments", async (req, res) => {
   });
 
   if (!place) {
-    res
+    return res
       .status(404)
       .json({ message: `Could not find place with id "${placeId}"` });
   }
 
-  const author = await User.findOne({
-    where: { userId: req.body.authorId },
-  });
-
-  if (!author) {
-    res
+  if (!req.currentUser) {
+    return res
       .status(404)
-      .json({
-        message: `Could not find author with id "${req.body.authorId}"`,
-      });
+      .json({ message: `You must be logged in to leave a rand or rave.` });
   }
 
   const comment = await Comment.create({
     ...req.body,
+    authorId: req.currentUser.userId,
     placeId: placeId,
   });
 
   res.send({
     ...comment.toJSON(),
-    author,
+    author: req.currentUser,
   });
 });
 
@@ -140,6 +150,12 @@ router.delete("/:placeId/comments/:commentId", async (req, res) => {
         .status(404)
         .json({
           message: `Could not find comment with id "${commentId}" for place with id "${placeId}"`,
+        });
+    } else if (comment.authorId !== req.currentUser?.userId) {
+      res
+        .status(403)
+        .json({
+          message: `You do not have permission to delete comment "${comment.commentId}"`,
         });
     } else {
       await comment.destroy();
--- backend/migrations/20210721052416-create-place.js
+++ backend/migrations/20210721052416-create-place.js
@@ -1,42 +1,42 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('places', {
+    await queryInterface.createTable("places", {
       place_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       city: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       state: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       cuisines: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       pic: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       founded: {
         type: Sequelize.INTEGER,
       },
       created_at: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updated_at: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('places');
-  }
+    await queryInterface.dropTable("places");
+  },
 };
